b857f5b028416d1d82d3fca32d7c8d185b614b57
Renzo Martin Rosas Vidal <renzo.rosas@servexternos.grupoavalon.es>
2022-12-12 16:08:02 -0500

    agregar register screen y example screen v2

--- lib/example_screen.dart
+++ lib/example_screen.dart
@@ -9,9 +9,66 @@ class ExampleScreen extends StatelessWidget {
       body: Column(
         children: [
           Image.network(
-              "https://www.pixelstalk.net/wp-content/uploads/2016/12/Beautiful-Landscape-Background-for-PC-768x480.jpg")
+              "https://www.pixelstalk.net/wp-content/uploads/2016/12/Beautiful-Landscape-Background-for-PC-768x480.jpg"),
+          SizedBox(height: 25),
+          textPlacesPoints(),
+          SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  Icon(Icons.call, color: Colors.blue),
+                  SizedBox(height: 8),
+                  Text("CALL", style: TextStyle(color: Colors.blue))
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(Icons.route, color: Colors.blue),
+                  SizedBox(height: 8),
+                  Text("ROUTE", style: TextStyle(color: Colors.blue))
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(Icons.share, color: Colors.blue),
+                  SizedBox(height: 8),
+                  Text("SHARE", style: TextStyle(color: Colors.blue))
+                ],
+              )
+            ],
+          )
         ],
       ),
+    );
+  }
+
+  Widget textPlacesPoints() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Oeschinen Lake Campground",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+            SizedBox(height: 10),
+            Text("Kandersteg, Switfzerland",
+                style: TextStyle(
+                    fontWeight: FontWeight.w300, color: Colors.grey.shade500))
+          ],
+        ),
+        SizedBox(width: 70),
+        //ROW ICONO ESTRELLA Y TEXTO 41
+        Row(
+          children: [
+            Icon(Icons.stars, color: Colors.red),
+            SizedBox(width: 4),
+            Text("41")
+          ],
+        )
+      ],
     );
   }
 }
